90c30d913c525a7ca544fb6ee286c330389b7a92
nkrumahthis <nkrumahthis@gmail.com>
2020-10-29 07:00:04 +0000

    initial commit

---
+++ app/src/main/java/com/nkrumahsarpong/waakyeapp/Spec.java
@@ -0,0 +1,19 @@
+package com.nkrumahsarpong.waakyeapp;
+
+public class Spec {
+    int id;
+    String string;
+
+    public Spec(int id, String string) {
+        this.id = id;
+        this.string = string;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getString() {
+        return string;
+    }
+}
